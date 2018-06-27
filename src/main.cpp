81f1d30e2d99d913fe45022f77cd336b71609488
Tyler Riddle <cardboardaardvark@gmail.com>
2018-06-26 23:35:23 -0700

    now using C++11 features

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv) {
     std::cout << "I started executing\n";
 
-    FlexLexer* lexer = new yyFlexLexer;
+    auto *lexer = new yyFlexLexer;
     while(lexer->yylex() != 0)
         ;
 
