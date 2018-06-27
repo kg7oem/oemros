419df9b2e2e2d7b29a066f7fe6de3af8cf6add5f
Tyler Riddle <cardboardaardvark@gmail.com>
2018-06-26 22:48:07 -0700

    flex is starting to work

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    std::cout << "I'm running\n";
+    std::cout << "I started executing\n";
+
+    FlexLexer* lexer = new yyFlexLexer;
+    while(lexer->yylex() != 0)
+        ;
+
     exit(1);
 }
