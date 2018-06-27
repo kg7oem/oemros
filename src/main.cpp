5b8b8a051f59d2561d88cf2db8379c1ef4818085
Tyler Riddle <cardboardaardvark@gmail.com>
2018-06-26 22:34:25 -0700

    fix whitespace

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <FlexLexer.h>
 #include <iostream>
 
-    int main(int argc, char **argv) {
-        std::cout << "I'm running\n";
-        exit(1);
-    }
+int main(int argc, char **argv) {
+    std::cout << "I'm running\n";
+    exit(1);
+}
